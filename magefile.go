cf393412aeb76e8ee4813d96e84ee9ac3cbbe48d
mtojek <marcin.tojek@elastic.co>
2020-05-13 12:51:50 +0200

    Use packages from package-storage

--- magefile.go
+++ magefile.go
@@ -30,14 +30,21 @@ var (
 	// GoLicenserImportPath controls the import path used to install go-licenser.
 	GoLicenserImportPath = "github.com/elastic/go-licenser"
 
-	publicDir    = "./public"
-	buildDir     = "./build"
-	packagePaths = []string{"./dev/packages/alpha/", "./dev/packages/example/", "./dev/packages/beats/"}
-	tarGz        = true
+	publicDir      = "./public"
+	buildDir       = "./build"
+	storageDir     = "./dev/packages/storage"
+	storageRepoDir = filepath.Join(buildDir, "package-storage")
+	packagePaths   = []string{"./dev/packages/storage/", "./dev/packages/beats/"}
+	tarGz          = true
 )
 
 func Build() error {
 	err := BuildPublicDirectory()
+	if err != nil {
+		return err
+	}
+
+	err = fetchPackageStorage()
 	if err != nil {
 		return err
 	}
@@ -72,6 +79,44 @@ func BuildPublicDirectory() error {
 		return err
 	}
 	return nil
+}
+
+func fetchPackageStorage() error {
+	err := os.RemoveAll(storageDir)
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll(storageRepoDir)
+	if err != nil {
+		return err
+	}
+
+	err = sh.Run("git", "clone", "https://github.com/elastic/package-storage.git", storageRepoDir)
+	if err != nil {
+		return err
+	}
+
+	packageStorageRevision := os.Getenv("PACKAGE_STORAGE_REVISION")
+	if packageStorageRevision == "" {
+		packageStorageRevision = "master"
+	}
+
+	err = sh.Run("git",
+		"--git-dir", filepath.Join(storageRepoDir, ".git"),
+		"--work-tree", storageRepoDir,
+		"checkout",
+		packageStorageRevision)
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(filepath.Join(storageRepoDir, "packages"), storageDir)
+	if err != nil {
+		return err
+	}
+
+	return os.RemoveAll(storageRepoDir)
 }
 
 func ImportBeats() error {
@@ -216,12 +261,7 @@ func Clean() error {
 		return err
 	}
 
-	err = os.RemoveAll(publicDir)
-	if err != nil {
-		return err
-	}
-
-	return os.Remove("package-registry")
+	return os.RemoveAll(publicDir)
 }
 
 func Vendor() error {
