f5fdc82b793a71d5b154dcd75c3a61a2283c6e5c
mtojek <marcin.tojek@elastic.co>
2020-05-29 12:23:50 +0200

    Fix: double query

--- dev/import-beats/kibana.go
+++ dev/import-beats/kibana.go
@@ -355,7 +355,7 @@ func stripReferencesToEventModuleInFilter(object mapStr, filterKey, moduleName s
 				return nil, errors.Wrapf(err, "setting meta.type failed")
 			}
 
-			_, err = filterObject.put("meta.value", fmt.Sprintf("{\"query\":{\"prefix\":{\"stream.dataset\":\"%s.\"}}}", moduleName))
+			_, err = filterObject.put("meta.value", fmt.Sprintf("{\"prefix\":{\"stream.dataset\":\"%s.\"}}", moduleName))
 			if err != nil {
 				return nil, errors.Wrapf(err, "setting meta.value failed")
 			}
@@ -366,10 +366,8 @@ func stripReferencesToEventModuleInFilter(object mapStr, filterKey, moduleName s
 			}
 
 			q := map[string]interface{}{
-				"query": map[string]interface{}{
-					"prefix": map[string]interface{}{
-						"stream.dataset": moduleName + ".",
-					},
+				"prefix": map[string]interface{}{
+					"stream.dataset": moduleName + ".",
 				},
 			}
 			_, err = filterObject.put("query", q)
