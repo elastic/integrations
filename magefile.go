b56f8790bce49fb3df1dc43972c27a36a4990f91
Shaunak Kashyap <ycombinator@gmail.com>
2020-10-26 23:01:28 -0400

    [CI] Collect tests results (#315)
    * Collect test results
    
    * Updating elastic-package dependency
    
    * DEBUGGING: Remove "empty" test report files
    
    * DEBUGGING: List files before and after deletion
    
    * DEBUGGING: fixing command so we can see file sizes
    
    * Undo debugging commits
    
    * TESTING: causing test failure
    
    * Revert "TESTING: causing test failure"
    
    This reverts commit 687d7f8bd98893ad33ccdd4db8ffe6188dc6c283.

--- magefile.go
+++ magefile.go
@@ -226,11 +226,11 @@ func Test() {
 }
 
 func testPipeline() error {
-	return runElasticPackageOnAllIntegrations("test", "pipeline")
+	return runElasticPackageOnAllIntegrations("test", "pipeline", "--report-format", "xUnit", "--report-output", "file")
 }
 
 func testSystem() error {
-	return runElasticPackageOnAllIntegrations("test", "system")
+	return runElasticPackageOnAllIntegrations("test", "system", "--report-format", "xUnit", "--report-output", "file")
 }
 
 func bootUpStack() error {
