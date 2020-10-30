bb74a610a3c60676ff6677622634ed76abb4ed54
Shaunak Kashyap <ycombinator@gmail.com>
2020-10-31 01:08:23 +0530

    Update Kibana asset IDs and titles during migration (#355)
    * Remove -ecs suffix from asset IDs
    
    * Handling collision case
    
    * Adding unit test
    
    * Remove " ECS" suffixes from textual fields
    
    * Update dashboard links across *all* assets of same package
    
    * Running mage format

---
+++ dev/import-beats/kibana_test.go
@@ -0,0 +1,57 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdateObjectID(t *testing.T) {
+	tests := map[string]struct {
+		OrigID     string
+		ModuleName string
+		ExpectedID string
+	}{
+		"no_ecs_suffix": {
+			"foo",
+			"bar",
+			"bar-foo",
+		},
+		"has_ecs_suffix": {
+			"foo-ecs",
+			"bar",
+			"bar-foo",
+		},
+		"has_pkgname_lowercase_prefix_and_ecs_suffix": {
+			"bar-foo-ecs",
+			"bar",
+			"bar-foo",
+		},
+		"has_pkgname_lowercase_prefix": {
+			"bar-foo",
+			"bar",
+			"bar-foo-pkg",
+		},
+		"has_pkgname_mixedcase_prefix_and_ecs_suffix": {
+			"bAr-foo-ecs",
+			"bar",
+			"bar-foo",
+		},
+		"has_pkgname_mixedcase_prefix": {
+			"BaR-foo",
+			"bar",
+			"bar-foo",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			actualID := updateObjectID(test.OrigID, test.ModuleName)
+			require.Equal(t, test.ExpectedID, actualID)
+		})
+	}
+}
