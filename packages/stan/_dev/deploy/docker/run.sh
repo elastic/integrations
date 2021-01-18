031585dfd8f01016ab4e49a73b9903672a4f10ea
ChrsMark <chrismarkou92@gmail.com>
2021-01-18 15:39:05 +0200

    final fixups
    Signed-off-by: ChrsMark <chrismarkou92@gmail.com>

--- packages/stan/_dev/deploy/docker/run.sh
+++ packages/stan/_dev/deploy/docker/run.sh
@@ -2,10 +2,8 @@
 
 /nats-streaming-server -DV -l /var/log/stan/stan.log -m 8222 &
 sleep 2
-while true; do /stan-bench -np 0 -ns 100 -qgroup T -n 100000000 -ms 1024 foo; done &
-while true; do /stan-bench -np 10 -ns 10 -n 1000000000 -ms 1024 bar; done &
-/stan-bench -np 0 -ns 100 -qgroup T -n 100000000 -ms 1024 fooZ &
-/stan-bench -np 10 -ns 10 -n 1000000000 -ms 1024 baz &
+#while true; do /stan-bench -np 0 -ns 100 -qgroup T -n 100000000 -ms 1024 foo; done &
+#while true; do /stan-bench -np 10 -ns 10 -n 1000000000 -ms 1024 bar; done &
 
 # Make sure the container keeps running
 tail -f /dev/null
