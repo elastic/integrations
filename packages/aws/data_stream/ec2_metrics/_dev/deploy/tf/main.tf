ae56884f2e6a47c4db86fd206e06075b635e9e01
Marcin Tojek <mtojek@users.noreply.github.com>
2021-02-01 18:17:54 +0100

    Define sample tests for AWS EC2 module (#599)
    * Define tests for AWS EC2 module
    
    * nit

---
+++ packages/aws/data_stream/ec2_metrics/_dev/deploy/tf/main.tf
@@ -0,0 +1,23 @@
+variable "TEST_RUN_ID" {
+  default = "detached"
+}
+
+provider "aws" {}
+
+resource "aws_instance" "i" {
+  ami           = data.aws_ami.latest-amzn.id
+  monitoring = true
+  instance_type = "t1.micro"
+  tags = {
+    Name = "elastic-package-test-${var.TEST_RUN_ID}"
+  }
+}
+
+data "aws_ami" "latest-amzn" {
+  most_recent = true
+  owners = [ "amazon" ] # AWS
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
