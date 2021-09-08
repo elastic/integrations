1e64fb4d1a2f3b188d15a8c18c432d94a557c4c2
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2021-09-08 13:33:59 +0200

    Add parameter to choose stack version to use in CI (#1585)
    Add parameter to Jenkinsfile to select a version of the stack
    to use for testing, and run a nightly build with master.

---
+++ .ci/schedule-daily.groovy
@@ -0,0 +1,40 @@
+@Library('apm@current') _
+
+pipeline {
+  agent none
+  environment {
+    NOTIFY_TO = credentials('notify-to')
+    PIPELINE_LOG_LEVEL = 'INFO'
+  }
+  options {
+    timeout(time: 2, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20', daysToKeepStr: '30'))
+    timestamps()
+    ansiColor('xterm')
+    disableResume()
+    durabilityHint('PERFORMANCE_OPTIMIZED')
+    rateLimitBuilds(throttle: [count: 60, durationName: 'hour', userBoost: true])
+    quietPeriod(10)
+  }
+  triggers {
+    cron('H H(2-5) * * *')
+  }
+  stages {
+    stage('Daily integration builds') {
+      steps {
+        build(
+          job: 'Ingest-manager/Integrations/master',
+          parameters: [stringParam(name: 'stackVersion', value: '7.x-SNAPSHOT')],
+          quietPeriod: 0,
+          wait: false,
+          propagate: false,
+        )
+      }
+    }
+  }
+  post {
+    cleanup {
+      notifyBuildResult(prComment: false)
+    }
+  }
+}
