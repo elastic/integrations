892b401cfbcb1ff62b81283ca81fbe6de484064a
Marcin Tojek <mtojek@users.noreply.github.com>
2022-01-26 13:44:17 +0100

    Schedule daily: test packages against 8.1 (#2603)
    * Schedule daily: test packages against 8.1
    
    * Bump up 7.16 to 7.17

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -22,22 +22,22 @@ pipeline {
     cron('H H(2-5) * * *')
   }
   stages {
-    stage('Daily integration builds with 7.16') {
+    stage('Daily integration builds with 7.17') {
       steps {
         build(
           job: env.INTEGRATION_JOB,
-          parameters: [stringParam(name: 'stackVersion', value: '7.16-SNAPSHOT')],
+          parameters: [stringParam(name: 'stackVersion', value: '7.17-SNAPSHOT')],
           quietPeriod: 0,
           wait: true,
           propagate: true,
         )
       }
     }
-    stage('Daily integration builds with 8.0') {
+    stage('Daily integration builds with 8.1') {
       steps {
         build(
           job: env.INTEGRATION_JOB,
-          parameters: [stringParam(name: 'stackVersion', value: '8.0-SNAPSHOT')],
+          parameters: [stringParam(name: 'stackVersion', value: '8.1-SNAPSHOT')],
           quietPeriod: 0,
           wait: true,
           propagate: true,
