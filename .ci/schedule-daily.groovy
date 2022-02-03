61b53a5bb97e0f082eb86222a1b759b56cb2991a
Marcin Tojek <mtojek@users.noreply.github.com>
2022-02-03 11:29:24 +0100

    Fix daily schedule: bad groovy syntax (#2637)

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -22,27 +22,29 @@ pipeline {
     cron('H H(2-5) * * *')
   }
   stages {
-    parallel {
-      stage('Daily integration builds with 7.17') {
-        steps {
-          build(
-            job: env.INTEGRATION_JOB,
-            parameters: [stringParam(name: 'stackVersion', value: '7.17-SNAPSHOT')],
-            quietPeriod: 0,
-            wait: true,
-            propagate: true,
-          )
+    stage('Daily integration builds') {
+      parallel {
+        stage('with stack v7.17') {
+          steps {
+            build(
+              job: env.INTEGRATION_JOB,
+              parameters: [stringParam(name: 'stackVersion', value: '7.17-SNAPSHOT')],
+              quietPeriod: 0,
+              wait: true,
+              propagate: true,
+            )
+          }
         }
-      }
-      stage('Daily integration builds with 8.1.0') {
-        steps {
-          build(
-            job: env.INTEGRATION_JOB,
-            parameters: [stringParam(name: 'stackVersion', value: '8.1.0-SNAPSHOT')],
-            quietPeriod: 0,
-            wait: true,
-            propagate: true,
-          )
+        stage('with stack v8.1') {
+          steps {
+            build(
+              job: env.INTEGRATION_JOB,
+              parameters: [stringParam(name: 'stackVersion', value: '8.1.0-SNAPSHOT')],
+              quietPeriod: 0,
+              wait: true,
+              propagate: true,
+            )
+          }
         }
       }
     }
