e8bf157702eab6726fc34d8a1cb9b5711ea62d51
Darshan Lukhi <darshan.lukhi@elastic.co>
2022-04-11 12:55:25 +0530

    Initial commit for Cisco ESA

---
+++ packages/cisco_esa/_dev/deploy/docker/sample_logs/content_scanner.@123456789.s
@@ -0,0 +1,2 @@
+Fri Mar 25 20:50:10 2022 Info: PF: Starting multi-threaded Perceptive server (pid=17729)
+Fri Mar 25 20:50:10 2022 Info: PF: Restarting content_scanner service.
---
+++ packages/cisco_esa/_dev/deploy/docker/sample_logs/gui_logs.@123456789.s
@@ -0,0 +1,11 @@
+Fri Mar 25 20:50:10 2022 Info: req:1.128.3.4 user:admin id:2v10z5fEuDsvhdbVE6Ck 200 GET xxx.png HTTP/1.1 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36
+Fri Mar 25 20:50:10 2022 Info: req:1.128.3.4 user:- id:2v10z5fEuDsvhdbVE6Ck 200 GET xxx.png HTTP/1.1 -
+Fri Mar 25 20:50:10 2022 Info: Action: User admin logged out from session 5GPz0QDlfxUYQ0Y3PgYN beacuse of inactivity timeout
+Fri Mar 25 20:50:10 2022 Info: Session fRK3TSjzhHhoI9CV5Kvt user:admin expired
+Fri Mar 25 20:50:10 2022 Info: Session fRK3TSjzhHhoI9CV5Kvt from 1.128.3.4 not found Destination:/mail_policies/email_security_manager/incoming_mail_policies
+Fri Mar 25 20:50:10 2022 Info: SourceIP:1.128.3.4 Destination:/login Username:admin Privilege:admin session:5GPz0QDlfxUYQ0Y3PgYN Action: The HTTPS session has been established successfully.
+Fri Mar 25 20:50:10 2022 Info: PERIODIC REPORTS: No root directory for Periodic Reports Archive. Probably, running first time...
+Fri Mar 25 20:50:10 2022 Warning: Could not fetch current Virus Threat Level: OS error opening URL 'http://example.com/xxxxx/xxxxx.txt'
+Fri Mar 25 20:50:10 2022 Warning: SSL error with client 1.128.3.4:000 - (336151574, 'error:14094416:SSL routines:ssl3_read_bytes:sslv3 alert certificate unknown')
+Fri Mar 25 20:50:10 2022 Info: Error in https connection from host 1.128.3.4 port 000 - [Errno 54] Connection reset by peer
+Fri Mar 25 20:50:10 2022 Info: Passphrase has been changed for user admin
