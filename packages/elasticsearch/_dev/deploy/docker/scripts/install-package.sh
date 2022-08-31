e80607d4e02facb06e35d852cc2db3f31c76c002
klacabane <kevin.lacabane@elastic.co>
2022-08-31 11:57:17 +0200

    install-package script updates

--- packages/elasticsearch/_dev/deploy/docker/scripts/install-package.sh
+++ packages/elasticsearch/_dev/deploy/docker/scripts/install-package.sh
@@ -7,17 +7,17 @@
 # by the `elastic-package stack` command but ideally we'd create a dedicated, standalone
 # agent that we can fully control.
 
-set -e
+set -euo pipefail
 
 eval "$(elastic-package stack shellinit)"
 
-script_dir=$(dirname ${BASH_SOURCE[0]})
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 
-package_version=`cat $script_dir/../../../../manifest.yml | sed -nr 's/^version: (.*)$/\1/p'`
-es_service_container=`docker ps --filter "publish=9201" --format "{{print .Names}}"`
+package_version=$(sed -nr 's/^version: (.*)$/\1/p' < "manifest.yml")
+es_service_container=$(docker ps --filter "publish=9201" --format "{{print .Names}}")
 elastic_agent_logs_dir=/tmp/service_logs
 policy_name=script-generated-elasticsearch-policy
-package_policy_file=$script_dir/package-policy.json
+package_policy_file=_dev/deploy/docker/scripts/package-policy.json
 kibana_host=$ELASTIC_PACKAGE_KIBANA_HOST
 
 # these are the values of the es service started by `elastic-package service up`
@@ -34,17 +34,17 @@ echo Found elasticsearch service "$es_service_container"
 
 elastic-package install
 
-policy=`cat $package_policy_file |
-  sed "s~__PACKAGE_VERSION__~$package_version~g" |
+policy=$(
+  sed "s~__PACKAGE_VERSION__~$package_version~g" < "$package_policy_file" |
   sed "s~__INTEGRATION_POLICY_NAME__~$policy_name~g" |
   sed "s~__ELASTICSEARCH_HOST__~$elasticsearch_host~g" |
   sed "s~__ELASTICSEARCH_USERNAME__~$username~g" |
   sed "s~__ELASTICSEARCH_PASSWORD__~$password~g" |
-  sed "s~__ELASTIC_AGENT_LOGS_DIR__~$elastic_agent_logs_dir~g"`
+  sed "s~__ELASTIC_AGENT_LOGS_DIR__~$elastic_agent_logs_dir~g")
 
 echo Attaching elasticsearch to Agent policy
 
-curl --insecure -Ss -u $username:$password -X POST $kibana_host/api/fleet/package_policies \
+curl --insecure -Ss -u "$username:$password" -X POST "$kibana_host/api/fleet/package_policies" \
   -H "kbn-xsrf: 1" \
   -H "Content-Type: application/json" \
   -d "$policy"
