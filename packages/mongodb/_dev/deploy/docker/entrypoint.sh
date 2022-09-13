55480d89e3ff226e5de611999f156b0c291461b3
manoj shetty <manojs.shetty@elastic.co>
2022-09-13 16:47:04 +0530

    updated dockerfile to support CI log file mount 	modified:   packages/mongodb/_dev/deploy/docker/Dockerfile 	modified:   packages/mongodb/_dev/deploy/docker/docker-compose.yml 	modified:   packages/mongodb/_dev/deploy/docker/entrypoint.sh

--- packages/mongodb/_dev/deploy/docker/entrypoint.sh
+++ packages/mongodb/_dev/deploy/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 touch /var/log/mongodb/mongod.log
 
-chown mongodb:mongodb /var/log/mongodb/mongod.log
+#chown mongodb:mongodb /var/log/mongodb/mongod.log
 
 chmod a+wx /var/log/mongodb
 chmod a+r -R /var/log/mongodb
