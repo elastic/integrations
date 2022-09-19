1db0bb04b6335d0ae081dd9e4e3ec91e5c5b88e1
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2022-09-19 12:56:18 +0200

    Use .env vars

--- packages/kibana/_dev/deploy/docker/scripts/setup.sh
+++ packages/kibana/_dev/deploy/docker/scripts/setup.sh
@@ -5,7 +5,7 @@
 set -e
 
 until curl --request POST $ES_SERVICE_HOST/_security/user/kibana_system/_password \
-  --user elastic:changeme \
+  --user elastic:$ELASTIC_PASSWORD \
   --header 'Content-Type: application/json' \
   --data "{\"password\":\"$KIBANA_PASSWORD\"}"
 do sleep 10; 
