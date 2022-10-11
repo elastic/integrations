25d2f64f202f3e7cbf8bf9bef7d328131205d205
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2022-10-11 18:27:32 +0200

    Add system tests for elasticsearch package
    (cherry picked from commit f02544d517fbf57827bc43f46265896656e96adb)

--- packages/elasticsearch/_dev/deploy/docker/scripts/generate-logs.sh
+++ packages/elasticsearch/_dev/deploy/docker/scripts/generate-logs.sh
@@ -32,6 +32,72 @@ curl --request PUT \
 }
 }'
 
+# set machine learning job
+curl --request PUT \
+  --url $ES_SERVICE_HOST/_ml/anomaly_detectors/test-job1?pretty \
+  --header "Authorization: Basic $auth" \
+  --header 'Content-Type: application/json' \
+  --header 'X-Opaque-ID: myApp1' \
+  --header 'traceparent: 00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01' \
+  --data '{
+            "analysis_config": {
+              "bucket_span": "15m",
+              "detectors": [
+                {
+                  "detector_description": "Sum of bytes",
+                  "function": "sum",
+                  "field_name": "bytes"
+                }
+              ]
+            },
+            "data_description": {
+              "time_field": "timestamp",
+              "time_format": "epoch_ms"
+            },
+            "analysis_limits": {
+              "model_memory_limit": "11MB"
+            },
+            "model_plot_config": {
+              "enabled": true,
+              "annotations_enabled": true
+            },
+            "results_index_name": "test-job1",
+            "datafeed_config":
+            {
+              "indices": [
+                "kibana_sample_data_logs"
+              ],
+              "query": {
+                "bool": {
+                  "must": [
+                    {
+                      "match_all": {}
+                    }
+                  ]
+                }
+              },
+              "runtime_mappings": {
+                "hour_of_day": {
+                  "type": "long",
+                  "script": {
+                    "source": "emit(doc['timestamp'].value.getHour());"
+                  }
+                }
+              },
+              "datafeed_id": "datafeed-test-job1"
+            }
+          }'
+
+## Open ML job
+curl --request POST \
+  --url $ES_SERVICE_HOST/_ml/anomaly_detectors/test-job1/_open \
+  --header "Authorization: Basic $auth" \
+  --header 'Content-Type: application/json' \
+  --header 'X-Opaque-ID: myApp1' \
+  --header 'traceparent: 00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01'
+
+
+
 while true
 do
   echo Generating slowlogs, audit and deprecation
