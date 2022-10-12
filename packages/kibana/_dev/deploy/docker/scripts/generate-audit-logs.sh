8196c200b3f54284e24a048a7654499100fdb584
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2022-10-12 18:19:17 +0200

    Fix docker-compose setup and use Hostname env var on test config

--- packages/kibana/_dev/deploy/docker/scripts/generate-audit-logs.sh
+++ packages/kibana/_dev/deploy/docker/scripts/generate-audit-logs.sh
@@ -1,24 +1,52 @@
 #!/bin/sh
 
 # Makes requests to kibana API so that audit logs can be generated
-
 set -e
 
-until curl --request GET \
-    --user "elastic:$KIBANA_PASSWORD" \
-    --url $KIBANA_SERVICE_HOST/login  \
-    --header 'Content-Type: application/json'
-do sleep 10;
-done;
+# Copy the log files from this container to /var/log/ which is a bind mount of ${SERVICE_LOGS_DIR}
+# This sh must be executed by a root user in order to have permission to write in the ${SERVICE_LOGS_DIR} folder
+copy_log_files () {
+  for f in /logs/*;
+  do
+    echo "Copy ${f##*/} file..."
+
+    if [[ ! -e /var/log/${f##*/} ]]; then
+      touch /var/log/${f##*/}
+    fi
+
+    cat "$f" >> /var/log/${f##*/}
+  done
+}
+
+attempt_counter=0
+max_attempts=6
+
+until curl -s --request GET \
+  --url $KIBANA_SERVICE_HOST/login \
+  --user "elastic:$KIBANA_PASSWORD" \
+  --header 'Content-Type: application/json'
+do
+
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo "Max attempts reached"
+    exit 1
+  fi
+
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 10
+done
 
 while true
 do
-  echo Generating audit logs
-
-  curl --request GET \
+  curl -s --request GET \
+    --url $KIBANA_SERVICE_HOST/api/features \
     --user "elastic:$KIBANA_PASSWORD" \
-    --url $KIBANA_SERVICE_HOST/api/features  \
     --header 'Content-Type: application/json'
 
+  echo "Audit log created"
+
+  copy_log_files
+
   sleep 10
-done;
+done
