205bb481bdde8ae00656824ad346ba949beb7d10
Carlos Crespo <crespocarlos@users.noreply.github.com>
2022-11-01 09:11:31 +0100

    Add system tests to elasticsearch package (#4442)
    * Add system tests for elasticsearch package
    
    (cherry picked from commit f02544d517fbf57827bc43f46265896656e96adb)
    
    * Update README
    
    * Revert changes to kibana package
    
    * Revert changes to server pipeline test
    
    * Fix gc static test
    
    * Set default env var value
    
    * Fix log generation
    
    * Replace .env with variants.yml

--- packages/elasticsearch/_dev/deploy/docker/scripts/generate-logs.sh
+++ packages/elasticsearch/_dev/deploy/docker/scripts/generate-logs.sh
@@ -3,10 +3,25 @@
 # Sends queries to the elasticsearch service configured in _dev/deploy in order
 # to generate all existing log types. `server` and `gc` logs will be generated
 # without external trigger.
-
 set -e
 
 auth=$(echo -n $ES_SERVICE_USERNAME:$ES_SERVICE_PASSWORD | base64)
+
+# Copy the log files content from this container to /var/log/ which is a bind mounted to ${SERVICE_LOGS_DIR}
+# This sh must be executed by a root user in order to have permission to write in the ${SERVICE_LOGS_DIR} folder
+copy_log_files () {
+  for f in /es_logs/*;
+  do
+    echo "Copy ${f##*/} file..."
+
+    if [[ ! -e /var/log/${f##*/} ]]; then
+      touch /var/log/${f##*/}
+    fi
+
+    ## appends only new lines
+    comm -23 "$f" /var/log/${f##*/} >> /var/log/${f##*/}
+  done
+}
 
 # create an index that will trace every indexing/searching operations
 curl --request PUT \
@@ -31,6 +46,72 @@ curl --request PUT \
 	"index.indexing.slowlog.threshold.index.trace": 0
 }
 }'
+
+# set machine learning job
+curl --request PUT \
+  --url $ES_SERVICE_HOST/_ml/anomaly_detectors/test-job1?pretty \
+  --header "Authorization: Basic $auth" \
+  --header 'Content-Type: application/json' \
+  --header 'X-Opaque-ID: myApp1' \
+  --header 'traceparent: 00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01' \
+  --data '{
+            "analysis_config": {
+              "bucket_span": "15m",
+              "detectors": [
+                {
+                  "detector_description": "Sum of bytes",
+                  "function": "sum",
+                  "field_name": "bytes"
+                }
+              ]
+            },
+            "data_description": {
+              "time_field": "timestamp",
+              "time_format": "epoch_ms"
+            },
+            "analysis_limits": {
+              "model_memory_limit": "11MB"
+            },
+            "model_plot_config": {
+              "enabled": true,
+              "annotations_enabled": true
+            },
+            "results_index_name": "test-job1",
+            "datafeed_config":
+            {
+              "indices": [
+                "kibana_sample_data_logs"
+              ],
+              "query": {
+                "bool": {
+                  "must": [
+                    {
+                      "match_all": {}
+                    }
+                  ]
+                }
+              },
+              "runtime_mappings": {
+                "hour_of_day": {
+                  "type": "long",
+                  "script": {
+                    "source": "emit(doc['timestamp'].value.getHour());"
+                  }
+                }
+              },
+              "datafeed_id": "datafeed-test-job1"
+            }
+          }'
+
+## Open ML job
+curl --request POST \
+  --url $ES_SERVICE_HOST/_ml/anomaly_detectors/test-job1/_open \
+  --header "Authorization: Basic $auth" \
+  --header 'Content-Type: application/json' \
+  --header 'X-Opaque-ID: myApp1' \
+  --header 'traceparent: 00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01'
+
+
 
 while true
 do
@@ -124,6 +205,8 @@ do
           }
       }
   }'
+
+  copy_log_files
 
   sleep 10
 done
