cb782279c1a8b6ce850554f7d066d86e3bfb0a85
Bharat Pasupula <saibharatchandra.pasupula@elastic.co>
2023-07-25 13:03:51 +0200

    Put command-script log

--- packages/sentinel_one_cloud_funnel/_dev/deploy/tf/main.tf
+++ packages/sentinel_one_cloud_funnel/_dev/deploy/tf/main.tf
@@ -70,12 +70,12 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.bucket.id
   key    = "new_object_key"
-  source = "/workspace/main.tf"
+  source = "./test-command-script.log"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag       = filemd5("/workspace/main.tf")
+  etag       = filemd5("./test-command-script.log")
   depends_on = [aws_sqs_queue.queue]
 }
 
