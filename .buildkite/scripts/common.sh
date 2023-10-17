8cc8602caff160b01a639c212fa10e55370603ff
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-10-17 19:30:51 +0200

    Sort folders

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -429,7 +429,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {}
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort
 }
 
 check_install_and_test_package() {
