cff69048e99bd974e66f1c2eca5e63d535aba004
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-10-18 19:29:52 +0200

    Test all packages observability

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -441,7 +441,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort  |egrep "netskope|logstash"
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort  #|egrep "netskope|logstash"
 }
 
 check_package() {
