468d78df86a4db0a4e25994a50c67f06f9dce92b
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-10-27 13:32:36 +0200

    Add scripts to test one package

---
+++ .buildkite/scripts/test_one_package.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+# default values
+SERVERLESS=false
+
+# used in common.sh
+FORCE_CHECK_ALL=${FORCE_CHECK_ALL:-"false"}
+
+if [ ! -d packages ]; then
+    echo "Missing packages folder"
+    if running_on_buildkite ; then
+        buildkite-agent annotate "Missing packages folder" --style "error"
+    fi
+    exit 1
+fi
+
+add_bin_path
+
+with_yq
+with_mage
+with_docker_compose
+with_kubernetes
+
+use_elastic_package
+
+prepare_stack
+
+check_package ${package}
---
+++ .buildkite/scripts/trigger_integrations_in_parallel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+pushd packages > /dev/null
+PACKAGE_LIST=$(list_all_directories | head -n 10)
+popd > /dev/null
+
+echo "steps:"
+echo "  - group: \":terminal: Test integrations\""
+echo "    key: \"integration-tests\""
+echo "    steps:"
+
+for package in ${PACKAGE_LIST}; do
+    echo "  - label: \"Check integrations ${package}\""
+    echo "    key: \"test-integrations-${package}\""
+    echo "    command: \".buildkite/scripts/run_integrations_sync.sh\""
+    echo "    agents:"
+    echo "      provider: gcp"
+    echo "    artifact_paths:"
+    echo "      - build/results/*.xml"
+done
+
