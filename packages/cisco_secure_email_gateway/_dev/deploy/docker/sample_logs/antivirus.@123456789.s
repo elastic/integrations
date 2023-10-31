f5d8cd39dcc8a84440cca4fbd1d63a4c8490dd1d
Mohit Jha <138874484+mohitjha-elastic@users.noreply.github.com>
2023-10-31 13:43:19 +0530

    [cisco_secure_email_gateway] Support New Format for Mail Category of logs and Add Pipeline for Antivirus (#8292)

---
+++ packages/cisco_secure_email_gateway/_dev/deploy/docker/sample_logs/antivirus.@123456789.s
@@ -0,0 +1,9 @@
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 69391938 - Result 'CLEAN' ()
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 68431780 0 - Error - 'Encrypted' '0x8004021'
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 66842418 0 - Virus 'CXmail/Phish-O' 'body.scan/Payment.html' 1 0
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 68325092 0 - Virus 'CXmail/MalPE-AC' 'body.scan/redactedFileName.rar' 1 0
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 66301278 0 - Virus 'CXmail/MalPE-HB' 'body.scan/anotherFileName.arj/anotherFileName.exe' 1 0
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 66298546 0 - Virus 'Mal/DrodRar-AIC' 'body.scan/anotherFileName.arj' 1 0
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 67753636 0 - Virus 'Troj/MSIL-TAR' 'body.scan/otherFileName.exe' 1 0
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 66710307 7 - Limit - 'Max Files Exceeded'
+Fri Sep 12 11:00:00 2023 Info: sophos  antivirus - MID 66708787 - timed out on message
