055b962fee4cdb14b32ecac9e137288c2d517311
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-11-02 13:10:35 +0100

    Check if no packages are going to be tested

--- .buildkite/scripts/trigger_integrations_in_parallel.sh
+++ .buildkite/scripts/trigger_integrations_in_parallel.sh
@@ -4,22 +4,19 @@ source .buildkite/scripts/common.sh
 
 set -euo pipefail
 
-# default values
-# echoerr "Some some envs"
-# env |egrep 'STACK_VERSION|SERVERLESS|FORCE_CHECK_ALL' || true 1>&2
-#
-# STACK_VERSION=${STACK_VERSION:-""}
-# SERVERLESS="false"
-# FORCE_CHECK_ALL=${FORCE_CHECK_ALL:-"false"}
-
 pushd packages > /dev/null
 PACKAGE_LIST=$(list_all_directories)
 popd > /dev/null
 
-echo "steps:"
-echo "  - group: \":terminal: Test integrations\""
-echo "    key: \"integration-tests\""
-echo "    steps:"
+PIPELINE_FILE="packages_pipeline.yml"
+touch packages_pipeline.yml
+
+echo "steps:" > ${PIPELINE_FILE}
+echo "  - group: \":terminal: Test integrations\"" >> ${PIPELINE_FILE}
+echo "    key: \"integration-tests\"" >> ${PIPELINE_FILE}
+echo "    steps:" >> ${PIPELINE_FILE}
+
+packages_to_test=0
 
 for package in ${PACKAGE_LIST}; do
     # check if needed to create an step for this package
@@ -35,14 +32,22 @@ for package in ${PACKAGE_LIST}; do
         continue
     fi
 
-    echo "    - label: \"Check integrations ${package}\""
-    echo "      key: \"test-integrations-${package}\""
-    echo "      command: \".buildkite/scripts/test_one_package.sh ${package}\""
-    echo "      agents:"
-    echo "        provider: gcp"
-    echo "      env:"
-    echo "        UPLOAD_SAFE_LOGS: 1"
-    echo "      artifact_paths:"
-    echo "        - build/test-results/*.xml"
-    echo "        - build/benchmark-results/*.xml"
+    packages_to_test=$((packages_to_test+1))
+    echo "    - label: \"Check integrations ${package}\"" >> ${PIPELINE_FILE}
+    echo "      key: \"test-integrations-${package}\"" >> ${PIPELINE_FILE}
+    echo "      command: \".buildkite/scripts/test_one_package.sh ${package}\"" >> ${PIPELINE_FILE}
+    echo "      agents:" >> ${PIPELINE_FILE}
+    echo "        provider: gcp" >> ${PIPELINE_FILE}
+    echo "      env:" >> ${PIPELINE_FILE}
+    echo "        UPLOAD_SAFE_LOGS: 1" >> ${PIPELINE_FILE}
+    echo "      artifact_paths:" >> ${PIPELINE_FILE}
+    echo "        - build/test-results/*.xml" >> ${PIPELINE_FILE}
+    echo "        - build/benchmark-results/*.xml" >> ${PIPELINE_FILE}
 done
+
+if [ ${packages_to_test} -eq 0 ]; then
+    buildkite-agent annotate "No packages to be tested" --context "ctx-no-packages" --style "warning"
+    exit 0
+fi
+
+cat ${PIPELINE_FILE} | buildkite-agent pipeline upload
