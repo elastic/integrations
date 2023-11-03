587411acfaeacafc2e6147dd875c006b1a46ef9d
Bharat Pasupula <saibharatchandra.pasupula@elastic.co>
2023-11-03 08:29:35 +0100

    Address PR comments

--- packages/cisco_umbrella/_dev/deploy/tf/main.tf
+++ packages/cisco_umbrella/_dev/deploy/tf/main.tf
@@ -50,12 +50,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.bucket.id
   key    = "new_object_key"
-  source = "/workspace/main.tf"
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag       = filemd5("/workspace/main.tf")
+  source = "./files/test-umbrella-proxylogs.log"
   depends_on = [aws_sqs_queue.queue]
 }
 
