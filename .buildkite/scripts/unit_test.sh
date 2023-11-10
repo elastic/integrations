8e1c42e19c79af4244eaa69f6e0907a9bfeb79dd
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-11-10 17:20:18 +0200

    test

--- .buildkite/scripts/unit_test.sh
+++ .buildkite/scripts/unit_test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-go test -covermode=atomic -coverprofile=build/TEST-go-integrations-coverage.cov -v -race -coverprofile=build/coverage.out ./... | tee build/test-unit.out
+go test -covermode=atomic -coverprofile=TEST-go-integrations-coverage.cov -v -race -coverprofile=coverage.out ./...
 
 # add_bin_path
 
