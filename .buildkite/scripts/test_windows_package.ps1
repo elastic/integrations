ee570b797002da71e3a077a36cadf613d1c78f2d
Marc Guasch <marc.guasch@elastic.co>
2023-11-10 15:58:10 +0100

    check docker

--- .buildkite/scripts/test_windows_package.ps1
+++ .buildkite/scripts/test_windows_package.ps1
@@ -25,6 +25,8 @@ go install github.com/jstemmer/go-junit-report
 go install gotest.tools/gotestsum
 go install github.com/elastic/elastic-package
 
+choco install docker-compose
+
 docker version
 docker compose version
 docker-compose version
