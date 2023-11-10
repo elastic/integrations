95d45a2d787def248304fc6c7a992326582fbdb6
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-11-10 18:17:16 +0200

    add junit

--- .buildkite/scripts/unit_test.sh
+++ .buildkite/scripts/unit_test.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-go test -covermode=atomic -coverprofile=TEST-go-integrations-coverage.cov -v -race -coverprofile=coverage.out ./...
+go test -covermode=atomic -coverprofile=TEST-go-integrations-coverage.cov -v -race -coverprofile=coverage.out ./... | tee test-unit.out
+
+go install github.com/jstemmer/go-junit-report@latest
+
+for file in *.out; do
+  go-junit-report > "${file}.xml" < ${file}
+done
