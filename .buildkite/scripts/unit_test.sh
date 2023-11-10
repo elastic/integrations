b8af8dc4974dcf4b8c11611ea95076e7f7143fe6
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-11-10 18:47:50 +0200

    test

--- .buildkite/scripts/unit_test.sh
+++ .buildkite/scripts/unit_test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-go test -covermode=atomic -coverprofile=TEST-go-integrations-coverage.cov -v -race -coverprofile=coverage.out ./... | tee test-unit.out
+go test -covermode=atomic -v -coverprofile=coverage.out ./... | tee test-unit.out
 
 go install github.com/jstemmer/go-junit-report@latest
 
