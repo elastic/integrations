46ddf09edc635e71578bc09787d16549ddcf119f
Marc Guasch <marc.guasch@elastic.co>
2023-11-13 13:41:34 +0100

    wip

--- .buildkite/scripts/test_windows_package.ps1
+++ .buildkite/scripts/test_windows_package.ps1
@@ -5,37 +5,31 @@ refreshenv
 
 $ErrorActionPreference = "Stop"
 
-# $env:GvmVersion = "0.5.2"
-# [Net.ServicePointManager]::SecurityProtocol = "tls12"
-# $env:GoVersion = Get-Content -Path .go-version
-# Invoke-WebRequest -URI https://github.com/andrewkroh/gvm/releases/download/v$env:GvmVersion/gvm-windows-amd64.exe -Outfile C:\Windows\System32\gvm.exe
-# gvm --format=powershell $env:GoVersion | Invoke-Expression
-# go version
+$env:GvmVersion = "0.5.2"
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+$env:GoVersion = Get-Content -Path .go-version
+Invoke-WebRequest -URI https://github.com/andrewkroh/gvm/releases/download/v$env:GvmVersion/gvm-windows-amd64.exe -Outfile C:\Windows\System32\gvm.exe
+gvm --format=powershell $env:GoVersion | Invoke-Expression
+go version
 
-# $GOPATH = $(go env GOPATH)
-# $env:Path = "$GOPATH\bin;" + $env:Path
-# [Environment]::SetEnvironmentVariable("GOPATH", "$GOPATH", [EnvironmentVariableTarget]::Machine)
-# [Environment]::SetEnvironmentVariable("Path", "$GOPATH\bin;$env:Path", [EnvironmentVariableTarget]::Machine)
+$GOPATH = $(go env GOPATH)
+$env:Path = "$GOPATH\bin;" + $env:Path
+[Environment]::SetEnvironmentVariable("GOPATH", "$GOPATH", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", "$GOPATH\bin;$env:Path", [EnvironmentVariableTarget]::Machine)
 
-# Install tools
-# go install github.com/magefile/mage
-# go install github.com/elastic/go-licenser
-# go install golang.org/x/tools/cmd/goimports
-# go install github.com/jstemmer/go-junit-report
-# go install gotest.tools/gotestsum
-# go install github.com/elastic/elastic-package
+Install tools
+go install github.com/magefile/mage
+go install github.com/elastic/go-licenser
+go install golang.org/x/tools/cmd/goimports
+go install github.com/jstemmer/go-junit-report
+go install gotest.tools/gotestsum
+go install github.com/elastic/elastic-package
 
 choco install docker-compose
 
-# Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
 
-# docker run hello-world
-
-whoami
-wsl --install
-wsl --version
-wsl -l
-wsl sh -c "whoami"
+docker run hello-world
 
 # elastic-package stack up -d -v
 
