4635f5909739f68dfb24c8c920ab42bb047ae178
Marc Guasch <marc.guasch@elastic.co>
2023-11-13 12:30:33 +0100

    cmd

--- .buildkite/scripts/test_windows_package.ps1
+++ .buildkite/scripts/test_windows_package.ps1
@@ -32,7 +32,7 @@ choco install docker-compose
 # docker run hello-world
 
 wsl --install
-wsl -e .\.buildkite\scripts\setup_docker_wsl.sh
+wsl bash -c "sudo apt install docker-ce docker-ce-cli containerd.io; sudo docker run hello-world"
 
 # elastic-package stack up -d -v
 
