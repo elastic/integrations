92900eb925c6295ad586af9ee8b6694d46c3e643
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-11-16 18:10:12 +0200

    fix the cycle

--- .buildkite/scripts/merge_xml.sh
+++ .buildkite/scripts/merge_xml.sh
@@ -6,7 +6,7 @@ sourceFolder="build/test-coverage"
 mergedCoverageFileName="${sourceFolder}/coverage_merged.xml"
 echo '<coverage>' > "${mergedCoverageFileName}"
 
-for file in "${sourceFolder}/*.xml"; do
+for file in ${sourceFolder}/*.xml; do
   [ "$file" = "${mergedCoverageFileName}" ] && continue
   sed '1d;$d' "$file" | awk '/<package /,/<\/package>/' >> "${mergedCoverageFileName}"
 done
