9d751a3298a84ba87bc22f1dac1e677e52fb4c39
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-11-16 10:26:44 +0100

    Add build step for publishing

---
+++ .buildkite/scripts/build_packages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+build_packages() {
+    pushd packages > /dev/null
+
+    for it in $(find . -maxdepth 1 -mindepth 1 -type d); do
+        local package
+        local version
+        local name
+        package=$(basename "${it}")
+        echo "Package ${package}: check"
+
+        pushd "${package}" > /dev/null
+
+        version=$(cat manifest.yml | yq .version)
+        name=$(cat manifest.yml | yq .name)
+
+        local package_zip="${name}-${version}.zip"
+
+        if is_already_published "${package_zip}" ; then
+            echo "Skipping. ${package_zip} already published"
+            popd > /dev/null
+            continue
+        fi
+
+        echo "Build package as zip: ${package}"
+        if check_and_build_package "${package}" ; then
+            unpublished="true"
+        else
+            report_build_failure "${package}"
+        fi
+        popd > /dev/null
+    done
+    popd > /dev/null
+}
+
+unpublished=false
+echo "--- Build packages"
+build_packages
+
+if [[ "${unpublished}" == "false" ]]; then
+    exit 0
+fi
+
+mkdir -p packages-to-sign/
+cp build/packages/*.zip packages-to-sign
---
+++ .buildkite/scripts/trigger_publish_packages.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+BUILD_PACKAGES_PATH="build/packages"
+
+## Support main pipeline and downstream pipelines
+pipeline_slug=${BUILDKITE_PIPELINE_SLUG}
+pipeline_build_number=${BUILDKITE_BUILD_NUMBER}
+
+if [ -n "$BUILDKITE_TRIGGERED_FROM_BUILD_PIPELINE_SLUG" ] ; then
+  pipeline_slug=$BUILDKITE_TRIGGERED_FROM_BUILD_PIPELINE_SLUG
+  pipeline_build_number=$BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER
+fi
+
+## Fail if no token
+if [ -z "$BUILDKITE_API_TOKEN" ] ; then
+  echo "Token could not be loaded from vault. Please review .buildkite/hooks/pre-command"
+  exit 1
+fi
+
+# pipeline_api_url="https://api.buildkite.com/v2/organizations/elastic/pipelines"
+# query_url="${pipeline_api_url}/$pipeline_slug/builds/$pipeline_build_number"
+# build_json=$(curl -sH "Authorization: Bearer $BUILDKITE_API_TOKEN" "${query_url}")
+#
+# GPG_SIGN_BUILD_ID=$(jq -r '.jobs[] | select(.step_key == "sign-service").triggered_build.id' <<< "$build_json")
+#
+# echo "--- Download signed artifacts"
+# mkdir -p ${BUILD_PACKAGES_PATH}
+# buildkite-agent artifact download --build "$GPG_SIGN_BUILD_ID" "*.*" ${BUILD_PACKAGES_PATH}/
+
+pushd "${BUILD_PACKAGES_PATH}" > /dev/null || exit 1
+
+echo "Rename asc to sig"
+for f in *.asc; do
+    mv "$f" "${f%.asc}.sig"
+done
+popd > /dev/null || exit 1
+
+buildkite-agent artifact upload build/packages/*.sig build/packages
+buildkite-agent artifact download build/packages/*.zip build/packages
+
+ls -l build/packages
+
+exit 0
+# for each package trigger a publish package
+PIPELINE_FILE="packages_pipeline.yml"
+touch packages_pipeline.yml
+
+cat <<EOF > ${PIPELINE_FILE}
+steps:
+  - group: ":package: Trigger publish pipeline"
+    key: "publish-packages"
+    steps:
+EOF
+
+for package_zip in *.zip ; do
+  if [ ! -f "${package_zip}.sig" ]; then
+      echo "Missing signature file for ${package_zip}"
+      continue
+  fi
+
+  package_name=$(basename "${package_zip}" .zip)
+  cat << EOF >> ${PIPELINE_FILE}
+    - label: "Publish package ${package_name}"
+      key: "publish-package-${package_name}"
+      trigger: "package-storage-infra-publishing"
+      concurrency: 1
+      concurrency_group: "integrations-trigger-publishing"
+      build:
+        env:
+          DRY_RUN: "true"
+          LEGACY_PACKAGE: "false"
+          PACKAGE_ZIP: "${package_zip}"
+EOF
+
+done
+popd > /dev/null || exit 1
