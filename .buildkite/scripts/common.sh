c460eaf2ff69ebaf5a3dccb0fdceaaaf01d5497e
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-11-21 15:12:34 +0100

    Test with a few packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -592,7 +592,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | head -n 10
 }
 
 check_package() {
