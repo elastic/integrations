636e44f3e83cd3024f06f7dfb7ed605c03f79dcd
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-12-04 12:57:14 +0100

    Remove step and deps to publish using Jenkins

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -212,22 +212,6 @@ with_github_cli() {
 }
 
 ## Logging and logout from Google Cloud
-google_cloud_upload_auth() {
-  local secretFileLocation
-  secretFileLocation=$(mktemp -d -p "${WORKSPACE}" -t "${TMP_FOLDER_TEMPLATE_BASE}.XXXXXXXXX")/${GOOGLE_CREDENTIALS_FILENAME}
-  echo "${PACKAGE_UPLOADER_GCS_CREDENTIALS_SECRET}" > "${secretFileLocation}"
-  gcloud auth activate-service-account --key-file "${secretFileLocation}" 2> /dev/null
-  export GOOGLE_APPLICATION_CREDENTIALS=${secretFileLocation}
-}
-
-google_cloud_signing_auth() {
-  local secretFileLocation
-  secretFileLocation=$(mktemp -d -p "${WORKSPACE}" -t "${TMP_FOLDER_TEMPLATE_BASE}.XXXXXXXXX")/${GOOGLE_CREDENTIALS_FILENAME}
-  echo "${SIGNING_PACKAGES_GCS_CREDENTIALS_SECRET}" > "${secretFileLocation}"
-  gcloud auth activate-service-account --key-file "${secretFileLocation}" 2> /dev/null
-  export GOOGLE_APPLICATION_CREDENTIALS=${secretFileLocation}
-}
-
 google_cloud_auth_safe_logs() {
     local gsUtilLocation
     gsUtilLocation=$(mktemp -d -p "${WORKSPACE}" -t "${TMP_FOLDER_TEMPLATE}")
