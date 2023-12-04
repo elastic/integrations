1104bef6a5ef85a14f90d5e72a02e34db7e097d4
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-12-04 15:39:50 +0200

    test the idea

--- .buildkite/scripts/merge_xml.sh
+++ .buildkite/scripts/merge_xml.sh
@@ -6,15 +6,16 @@ sourceFolder="build/test-coverage"
 mergedCoverageFileName="${sourceFolder}/coverage_merged.xml"
 echo '<coverage>' > "${mergedCoverageFileName}"
 
-for file in ${sourceFolder}/*.xml; do
+for file in ${sourceFolder}/*coverage-.xml; do
   [ "$file" = "${mergedCoverageFileName}" ] && continue
   sed '1d;$d' "$file" | awk '/<package /,/<\/package>/' >> "${mergedCoverageFileName}"
 done
 
 echo '</coverage>' >> "${mergedCoverageFileName}"
 
-file_to_copy=$(find ${sourceFolder} -maxdepth 1 -type f -name "coverage-.xml" | head -n 1)
-cp "$file_to_copy" coverage-reports/
+file_to_copy=$(find ${sourceFolder} -maxdepth 1 -type f -name "coverage-.xml" | head -n 1 | xargs basename)
+echo "Copy file ${file_to_copy} to the 'coverage-reports' folder"
+cp ${file_to_copy} coverage-reports/
 
 ls ${sourceFolder}
 ls coverage-reports
