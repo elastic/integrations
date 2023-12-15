c756bba4cc5e65646a066e5f19ea7384da13c6a0
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-12-15 07:53:02 +0200

    test

--- .buildkite/scripts/backport_branch.sh
+++ .buildkite/scripts/backport_branch.sh
@@ -28,7 +28,7 @@ else
   REMOVE_ALL_PACKAGES="${REMOVE_ALL_PACKAGES:-"false"}"
 fi
 
-if [[ -z "$PACKAGE_NAME" || -z "$PACKAGE_VERSION" ]]; then
+if [[ -z "$PACKAGE_NAME" ]] || [[ -z "$PACKAGE_VERSION" ]]; then
   buildkite-agent annotate "The variables **PACKAGE_NAME** or **PACKAGE_VERSION** aren't defined, please try again" --style "warning"
   exit 1
 fi
@@ -37,6 +37,7 @@ FULL_PACKAGE_NAME="${PACKAGE_NAME}-${PACKAGE_VERSION}"
 FULL_ZIP_PACKAGE_NAME="${PACKAGE_NAME}-${PACKAGE_VERSION}.zip"
 SOURCE_BRANCH="main"
 BACKPORT_BRANCH_NAME="backport-${PACKAGE_NAME}-${PACKAGE_VERSION}"
+PACKAGES_FOLDER_PATH="packages"
 
 isPackagePublished() {
   local packageZip=$1
@@ -58,20 +59,38 @@ isCommitExist() {
 
 createBackportBranch() {
   if [ git checkout -b $BACKPORT_BRANCH_NAME $BASE_COMMIT ]; then
-    echo "The branch $BACKPORT_BRANCH_NAME was created."
+    echo "The branch $BACKPORT_BRANCH_NAME has created."
   else
     buildkite-agent annotate "The backport branch $backport_branch_name wasn't created." --style "warning"
     exit 1
 }
 
-addCiFifes() {
+removeAllPackages() {
+  for dir in "$PACKAGES_FOLDER_PATH"/*; do
+    if [[ -d "$dir" ]] && [[ "$(basename "$dir")" != "$PACKAGE_NAME" ]]; then
+      echo "Removing directory: $dir"
+      rm -rf "$dir"
+    fi
+  done
+}
+
+processFifes() {
   local BUILDKITE_FOLDER_PATH=".buildkite"
   local JENKINSFILE_PATH=".ci\Jenkinsfile"
   git checkout $BACKPORT_BRANCH_NAME
+  echo "Copying $BUILDKITE_FOLDER_PATH..."
   git checkout $SOURCE_BRANCH -- $BUILDKITE_FOLDER_PATH
+  echo "Copying $JENKINSFILE_PATH..."
   git checkout $SOURCE_BRANCH $JENKINSFILE_PATH
   ls -la $BUILDKITE_FOLDER_PATH
   ls -la
+
+  if [ "${REMOVE_ALL_PACKAGES}" == "true" ]; then
+    echo "Removing all packages from $PACKAGES_FOLDER_PATH folder"
+    removeAllPackages
+  fi
+
+  echo "Commiting and pushing..."
   # git add .
   # git commit -m "Add $BUILDKITE_FOLDER_PATH and $JENKINSFILE_PATH to backport branch: $BACKPORT_BRANCH_NAME"
   # git push origin $BACKPORT_BRANCH_NAME
@@ -92,4 +111,7 @@ fi
 echo "Creating local backport-branch"
 createBackportBranch "${PACKAGE_NAME}" "${VERSION}" "${BASE_COMMIT}"
 
-addCiFifes
+echo "Adding CI files to the branch"
+processFifes
+
+buildkite-agent annotate "The backport branch: $BACKPORT_BRANCH_NAME has created. $BUILDKITE_FOLDER_PATH and $JENKINSFILE_PATH have added to the branch." --style "info"
