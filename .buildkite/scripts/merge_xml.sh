574ccf877fb476d089ca4ae9f9ce8d16299b422e
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-01-25 20:45:38 +0100

    Update script to merge XML files

--- .buildkite/scripts/merge_xml.sh
+++ .buildkite/scripts/merge_xml.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# Script to merge all the coverage XML files into just one file.
+# It supports XML files using generic test coverage report format:
+# https://docs.sonarsource.com/sonarqube/9.8/analyzing-source-code/test-coverage/generic-test-data/#generic-test-coverage
+
 sourceFolder="build/test-coverage"
 mergedCoverageFileName="coverage_merged.xml"
 
@@ -10,11 +14,11 @@ echo "Generating ${mergedCoverageFileName} into ${sourceFolder}..."
 echo '<?xml version="1.0" encoding="UTF-8"?>' > "${mergedCoverageFileName}"
 echo '<coverage version="1">' >> "${mergedCoverageFileName}"
 
-# for file in coverage-nginx-*.xml coverage-elastic_package_registry-*.xml; do
 for file in coverage-*.xml; do
-  [[ "$file" == "${mergedCoverageFileName}" ]] && continue
+  if [[ "$file" == "${mergedCoverageFileName}" ]]; then
+      continue
+  fi
   echo " - Adding ${file}"
-  # generic coverage
   sed '1d;$d' "$file" | awk '/<file/,/<\/file>/' >> "${mergedCoverageFileName}"
 done
 
