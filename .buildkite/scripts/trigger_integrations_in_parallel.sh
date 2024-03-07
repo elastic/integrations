e0996ed4b16e0c6fe6c6d0bf1cb0895023fc95b5
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-03-07 13:42:21 +0100

    Restore triggering all test packages

--- .buildkite/scripts/trigger_integrations_in_parallel.sh
+++ .buildkite/scripts/trigger_integrations_in_parallel.sh
@@ -31,40 +31,40 @@ echo "[DEBUG] Checking with commits: from: '${from}' to: '${to}'"
 
 packages_to_test=0
 
-# for package in ${PACKAGE_LIST}; do
-#     # check if needed to create an step for this package
-#     pushd "packages/${package}" > /dev/null
-#     skip_package="false"
-#     if ! reason=$(is_pr_affected "${package}" "${from}" "${to}") ; then
-#         skip_package="true"
-#     fi
-#     echoerr "${reason}"
-#     popd > /dev/null
-# 
-#     if [[ "$skip_package" == "true" ]] ; then
-#         continue
-#     fi
-# 
-#     packages_to_test=$((packages_to_test+1))
-#     cat << EOF >> ${PIPELINE_FILE}
-#     - label: "Check integrations ${package}"
-#       key: "test-integrations-${package}"
-#       command: ".buildkite/scripts/test_one_package.sh ${package} ${from} ${to}"
-#       agents:
-#         provider: gcp
-#       env:
-#         STACK_VERSION: "${STACK_VERSION}"
-#         FORCE_CHECK_ALL: "${FORCE_CHECK_ALL}"
-#         SERVERLESS: "false"
-#         UPLOAD_SAFE_LOGS: ${UPLOAD_SAFE_LOGS}
-#       artifact_paths:
-#         - build/test-results/*.xml
-#         - build/test-coverage/*.xml
-#         - build/benchmark-results/*.json
-#         - build/elastic-stack-dump/*/logs/*.log
-#         - build/elastic-stack-dump/*/logs/fleet-server-internal/**/*
-# EOF
-# done
+for package in ${PACKAGE_LIST}; do
+    # check if needed to create an step for this package
+    pushd "packages/${package}" > /dev/null
+    skip_package="false"
+    if ! reason=$(is_pr_affected "${package}" "${from}" "${to}") ; then
+        skip_package="true"
+    fi
+    echoerr "${reason}"
+    popd > /dev/null
+
+    if [[ "$skip_package" == "true" ]] ; then
+        continue
+    fi
+
+    packages_to_test=$((packages_to_test+1))
+    cat << EOF >> ${PIPELINE_FILE}
+    - label: "Check integrations ${package}"
+      key: "test-integrations-${package}"
+      command: ".buildkite/scripts/test_one_package.sh ${package} ${from} ${to}"
+      agents:
+        provider: gcp
+      env:
+        STACK_VERSION: "${STACK_VERSION}"
+        FORCE_CHECK_ALL: "${FORCE_CHECK_ALL}"
+        SERVERLESS: "false"
+        UPLOAD_SAFE_LOGS: ${UPLOAD_SAFE_LOGS}
+      artifact_paths:
+        - build/test-results/*.xml
+        - build/test-coverage/*.xml
+        - build/benchmark-results/*.json
+        - build/elastic-stack-dump/*/logs/*.log
+        - build/elastic-stack-dump/*/logs/fleet-server-internal/**/*
+EOF
+done
 
 if [ ${packages_to_test} -eq 0 ]; then
     buildkite-agent annotate "No packages to be tested" --context "ctx-no-packages" --style "warning"
