a2ca7ca316495fea1f8183ccf8e48c023c94db4e
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-04-23 16:54:01 +0200

    Add parameter to install

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -162,8 +162,8 @@ with_docker() {
     fi
     echo "deb [arch=${architecture} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu ${ubuntu_codename} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-downgrades -y "docker-ce=${debian_version}"
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-downgrades -y "docker-ce-cli=${debian_version}"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-change-held-packages --allow-downgrades -y "docker-ce=${debian_version}"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-change-held-packages --allow-downgrades -y "docker-ce-cli=${debian_version}"
     sudo systemctl start docker
 
     echo "- Installed docker client version:"
