43173369fb8e2c5bd4c6e5f7da16d0cf0325b675
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-05-16 10:46:06 +0200

    [Buildkite] Update annotation in backport pipeline (#9873)

--- .buildkite/scripts/backport_branch.sh
+++ .buildkite/scripts/backport_branch.sh
@@ -26,15 +26,15 @@ fi
 
 # Report data set in the input step
 PARAMETERS=(
-    "- **DRY_RUN**=$DRY_RUN"
-    "- **BASE_COMMIT**=$BASE_COMMIT"
-    "- **PACKAGE_NAME**=$PACKAGE_NAME"
-    "- **PACKAGE_VERSION**=$PACKAGE_VERSION"
-    "- **REMOVE_OTHER_PACKAGES**=$REMOVE_OTHER_PACKAGES"
+    "**DRY_RUN**=$DRY_RUN"
+    "**BASE_COMMIT**=$BASE_COMMIT"
+    "**PACKAGE_NAME**=$PACKAGE_NAME"
+    "**PACKAGE_VERSION**=$PACKAGE_VERSION"
+    "**REMOVE_OTHER_PACKAGES**=$REMOVE_OTHER_PACKAGES"
 )
 
 # Show each parameter in a different line
-echo "Parameters:\n${PARAMETERS[*]}" | tr ' ' '\n' | buildkite-agent annotate \
+echo "Parameters: ${PARAMETERS[*]}" | sed 's/ /\n- /g' | buildkite-agent annotate \
     --style "info" \
     --context "context-parameters"
 
