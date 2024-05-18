503368920f9769f66b2efc46d099fbd60c545b46
aliabbas-elastic <aliabbas.attarwala@elastic.co>
2024-05-18 14:54:08 +0530

    rewrite code in functions

--- packages/couchbase/_dev/deploy/docker/files/setup.sh
+++ packages/couchbase/_dev/deploy/docker/files/setup.sh
@@ -7,23 +7,39 @@ XDCR_CLUSTER_NAME="cluster"
 REPLICATION_TO_BUCKET="travel-sample"
 
 # wait for the couchbase-server starts
-until curl -f  http://$ADMIN_USER:$ADMIN_PASSWORD@$CB_HOST:$CB_PORT/pools/default
-do
-  sleep 5s
-done
+is_couchbase_ready() {
+  until curl -s -f http://$ADMIN_USER:$ADMIN_PASSWORD@$CB_HOST:$CB_PORT/pools/default >/dev/null 2>&1; do
+    sleep 5s
+  done
+}
+is_couchbase_ready
 
-# wait till "beer-sample" bucket is added from sampleBuckets
-until [ "$(curl -v -u $ADMIN_USER:$ADMIN_PASSWORD -X POST http://$CB_HOST:$CB_PORT/sampleBuckets/install -d "[\"$BUCKET_NAME\"]" -o /dev/null -w '%{http_code}')" -eq 202 ]
-do
-  sleep 5s
+# adds a Couchbase sample bucket using the provided BUCKET_NAME variable
+add_cb_sample_bucket() {
+  http_code=$(curl -s -o /dev/null -w "%{http_code}" \
+    --user "${ADMIN_USER}:${ADMIN_PASSWORD}" \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data "[\"${BUCKET_NAME}\"]" \
+    "http://${CB_HOST}:${CB_PORT}/sampleBuckets/install")
+
+  if [[ "${http_code}" -eq 202 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Loop until the sample bucket is successfully added
+while ! add_cb_sample_bucket; do
+  sleep 5
 done
 
 # using couchbase-cli run xdcr-setup for the cluster
 couchbase-cli xdcr-setup -c $CB_HOST -u $ADMIN_USER -p $ADMIN_PASSWORD --create --xdcr-cluster-name $XDCR_CLUSTER_NAME --xdcr-hostname $CB_HOST --xdcr-username $ADMIN_USER --xdcr-password $ADMIN_PASSWORD
 
 # wait till the "beer-sample" bucket is ready
-until [ "$(curl -s -w '%{http_code}' -o /dev/null "http://$ADMIN_USER:$ADMIN_PASSWORD@$CB_HOST:$CB_PORT/pools/default/buckets/$BUCKET_NAME/stats")" -eq 200 ]
-do
+until [ "$(curl -s -w '%{http_code}' -o /dev/null "http://$ADMIN_USER:$ADMIN_PASSWORD@$CB_HOST:$CB_PORT/pools/default/buckets/$BUCKET_NAME/stats")" -eq 200 ]; do
   sleep 5s
 done
 
