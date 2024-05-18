99cbe5696fcd44d0cc5d57b1ebc10a2b7ed9dfcb
aliabbas-elastic <aliabbas.attarwala@elastic.co>
2024-05-18 13:22:40 +0530

    add silent and fix comments

--- packages/couchbase/_dev/deploy/docker/files/setup.sh
+++ packages/couchbase/_dev/deploy/docker/files/setup.sh
@@ -13,7 +13,7 @@ done
 # using couchbase-cli run xdcr-setup for the cluster
 couchbase-cli xdcr-setup -c 127.0.0.1 -u Administrator -p password --create --xdcr-cluster-name cluster --xdcr-hostname 127.0.0.1 --xdcr-username Administrator --xdcr-password password
 
-# wait till the xdcr-setup creates cluster
+# wait till the "beer-sample" bucket is ready
 until [ "$(curl -s -w '%{http_code}' -o /dev/null "http://Administrator:password@localhost:8091/pools/default/buckets/beer-sample/stats")" -eq 200 ]
 do
   sleep 5s
