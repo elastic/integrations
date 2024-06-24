ea2b0d4a2a80e4712cc6f5ce07d27a470fb4848d
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-06-24 16:11:25 +0200

    Add script into pipeline

---
+++ .buildkite/scripts/report_issues.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+junit_folder="test-results"
+test_results_folder="build/${junit_folder}"
+buildkite_pattern="${test_results_folder}/*.xml"
+
+download_test_results() {
+    mkdir -p "${test_results_folder}"
+
+    if ! buildkite-agent artifact download "${buildkite_pattern}" . ; then
+      echo "[report] No test results generated"
+      return 1
+    fi
+    return 0
+}
+
+if running_on_buildkite ; then
+    echo "--- Installing tools"
+    add_bin_path
+    with_go
+    with_github_cli # to post comments in Pull Requests
+
+    echo "--- Download Test Results"
+    if ! download_test_results ; then
+        exit 0
+    fi
+fi
+
+echo "--- Report Issues"
+mage -v ReportIssues
+
