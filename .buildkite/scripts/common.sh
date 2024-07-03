d8618c18471ace2e97939afe72f63dcfce623ab6
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-07-03 16:46:58 +0200

    Debug first with a few packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -742,7 +742,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | grep -E 'nginx|network_traffic'
 }
 
 check_package() {
