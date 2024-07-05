cf57a973b2ea1577a44a9f6037f1bf8512527d08
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-07-05 14:28:51 +0200

    Test all packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -742,7 +742,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort |grep elastic_package_registry
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort
 }
 
 check_package() {
