0835d9193a4c6be9b0ba722596282403875ec478
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-08-22 12:57:24 +0200

    Show docker images

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -519,12 +519,12 @@ prepare_stack() {
     ${ELASTIC_PACKAGE_BIN} stack status
     echo ""
 
-    if [[ "${ELASTIC_AGENT_DOCKER_IMAGE:-""}" != "" ]]; then
+    # if [[ "${ELASTIC_AGENT_DOCKER_IMAGE:-""}" != "" ]]; then
         echo ""
         echo "Images used for Elastic Agent"
         docker ps --format "{{.Names}} {{.Image}}" |grep "elastic-agent"
         echo ""
-    fi
+    # fi
 }
 
 is_serverless() {
