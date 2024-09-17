9f78a9b2dfbe7ef9df1e15d0b8ca0ce723084d36
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-09-17 13:24:38 +0200

    [CI] [backport-security_detection_engine-8.12] Remove mage install deps (#11152)
    * Remove deps when installing go/mage
    
    * [CI] Fix script that retrieves oldest supported version (#11126)
    
    Fix find oldest supported version script. Major and minor numbers
    from versions were compared as strings, but they should be compared
    as integers.

--- .buildkite/scripts/build_packages.sh
+++ .buildkite/scripts/build_packages.sh
@@ -43,7 +43,7 @@ report_build_failure() {
 
     # if running in Buildkite , add an annotation
     if [ -n "${BUILDKITE_BRANCH+x}" ]; then
-        buildkite-agent annotate "Build package ${package} failed, not published." --ctx "ctx-build-${package}" --style "warning"
+        buildkite-agent annotate "Build package ${package} failed, not published." --context "ctx-build-${package}" --style "warning"
     fi
 }
 
