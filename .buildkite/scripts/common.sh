3d615b8a652c51f093dad67638ba98bb08a891a8
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-01-21 18:42:01 +0100

    Test with all packages using transforms

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -751,7 +751,8 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | grep -E '^ti_'
+    # find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | grep -E '^ti_'
+    find . -type f -name transform.yml | awk -F '/' '{print $2}' | sort  | uniq
 }
 
 check_package() {
