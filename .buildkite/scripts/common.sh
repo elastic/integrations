25cc4b883d93997881d5e66ff962fd7e9dfb2edb
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-02-25 20:16:58 +0100

    Move skip check to test_one_package

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -1018,12 +1018,6 @@ process_package() {
     fi
 
     popd > /dev/null
-    if [[ "${package}" == "elastic_connectors" ]]; then
-        # TODO: Remove this skip once elastic_connectors can be installed again
-        # For reference: https://github.com/elastic/kibana/pull/211419
-        echo "[${package}]: Skipped errors in package"
-        exit_code=0
-    fi
     return $exit_code
 }
 
