fb4a399fbd1effa0ca416e52c5ba8276bd30878a
Victor Martinez <victormartinezrubio@gmail.com>
2025-05-05 18:06:48 +0200

    docs: aws variables
    Co-authored-by: Mario Rodriguez Molins <mario.rodriguez@elastic.co>

--- .buildkite/scripts/trigger_integrations_in_parallel.sh
+++ .buildkite/scripts/trigger_integrations_in_parallel.sh
@@ -90,6 +90,7 @@ for package in ${PACKAGE_LIST}; do
         UPLOAD_SAFE_LOGS: ${UPLOAD_SAFE_LOGS}
       plugins:
         # See https://github.com/elastic/oblt-infra/blob/main/conf/resources/repos/integrations/01-aws-buildkite-oidc.tf
+        # This plugin creates the environment variables required by the service deployer (AWS_SECRET_ACCESS_KEY and AWS_SECRET_KEY_ID)
         - elastic/oblt-aws-auth#v0.1.0:
             duration: 3600 # seconds
       artifact_paths:
