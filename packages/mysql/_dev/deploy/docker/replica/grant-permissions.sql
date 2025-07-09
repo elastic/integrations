de254290ee50777584193e161e5d4313c8734420
subham sarkar <subham.sarkar@elastic.co>
2025-07-09 20:13:33 +0530

    Debug

--- packages/mysql/_dev/deploy/docker/replica/grant-permissions.sql
+++ packages/mysql/_dev/deploy/docker/replica/grant-permissions.sql
@@ -1,3 +1,4 @@
 CREATE USER IF NOT EXISTS 'mydb_replica_user'@'%' IDENTIFIED BY 'mydb_replica_pwd';
+GRANT REPLICATION SLAVE ON *.* TO 'mydb_replica_user'@'%';
 GRANT REPLICATION CLIENT ON *.* TO 'mydb_replica_user'@'%';
 FLUSH PRIVILEGES;
