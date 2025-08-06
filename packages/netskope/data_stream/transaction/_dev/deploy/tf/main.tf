4b1e28b87659732310f4b1bda23cabbacfad3819
moxarth-rathod <moxarth.rathod@elastic.co>
2025-08-06 13:14:40 +0530

    Add multiple system tests for Transaction data stream

--- packages/netskope/data_stream/transaction/_dev/deploy/tf/main.tf
+++ packages/netskope/data_stream/transaction/_dev/deploy/tf/main.tf
@@ -1,3 +1,33 @@
+# GCS Setup
+
+provider "google" {
+  default_labels = {
+  environment  = var.ENVIRONMENT
+  repo         = var.REPO
+  branch       = var.BRANCH
+  build        = var.BUILD_ID
+  created_date = var.CREATED_DATE
+  }
+}
+
+resource "google_storage_bucket" "netskope_transaction_bucket" {
+  name     = "elastic-package-gcs-bucket-${var.TEST_RUN_ID}"
+  location = var.BUCKET_REGION
+}
+# See https://github.com/elastic/oblt-infra/blob/main/conf/resources/repos/integrations/01-gcp-buildkite-oidc.tf
+
+resource "google_storage_bucket_object" "netskope_transaction_bucket_object" {
+  name   = var.OBJECT_NAME
+  bucket = google_storage_bucket.netskope_transaction_bucket.name
+  source = var.FILE_PATH
+}
+
+output "netskope_transaction_bucket_name" {
+  value = google_storage_bucket.netskope_transaction_bucket.name
+}
+
+# AWS Setup
+
 provider "aws" {
   region = "us-east-1"
   default_tags {
