4af9509e3792a203d896601e6e4e1215c6d42cb2
stefans-elastic <stefan.stas@elastic.co>
2025-10-13 14:25:27 +0300

    cover kafka 0.10.2.1 and kafka 1.1.0 in system test variants

--- packages/kafka/_dev/deploy/docker/zookeeper/healthcheck.sh
+++ packages/kafka/_dev/deploy/docker/zookeeper/healthcheck.sh
@@ -3,13 +3,13 @@
 set -e
 
 # 1. Check for the Kafka Broker process
-pgrep -f 'kafka.Kafka' > /dev/null || { echo "Healthcheck failed: Kafka broker not running."; exit 1; }
+ps -ef | grep '[k]afka.Kafka' | awk '{print $2}' > /dev/null || { echo "Healthcheck failed: Kafka broker not running."; exit 1; }
 
 # 2. Check for the Console Producer process
-pgrep -f 'kafka.tools.ConsoleProducer' > /dev/null || { echo "Healthcheck failed: Console producer not running."; exit 1; }
+ps -ef | grep '[k]afka.tools.ConsoleProducer' | awk '{print $2}' > /dev/null || { echo "Healthcheck failed: Console producer not running."; exit 1; }
 
 # 3. Check for the Console Consumer process
-pgrep -f 'kafka.tools.ConsoleConsumer' > /dev/null || { echo "Healthcheck failed: Console consumer not running."; exit 1; }
+ps -ef | grep '[k]afka.tools.ConsoleConsumer' | awk '{print $2}' > /dev/null || { echo "Healthcheck failed: Console consumer not running."; exit 1; }
 
 # If all checks pass, exit with 0 to indicate the container is healthy.
 exit 0
