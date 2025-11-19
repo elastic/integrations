1978834c8c57779ae6e0761c4af890261332117f
Maxim Palenov <maxim.palenov@elastic.co>
2025-11-19 17:13:31 +0100

    handle constrain parsing errors

--- .buildkite/scripts/packages/security_detection_engine.sh
+++ .buildkite/scripts/packages/security_detection_engine.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+if [[ "${BUILDKITE_PULL_REQUEST}" == "false" ]]; then
+  exit 0
+fi
+
 # Fetch active Kibana versions
 ACTIVE_KIBANA_VERSIONS=$(curl -sL https://raw.githubusercontent.com/elastic/kibana/main/versions.json | yq '.versions[].version' | xargs)
 echo "Active Kibana versions: $ACTIVE_KIBANA_VERSIONS"
@@ -18,14 +22,19 @@ cat <<'GO' > "$SEMVER_FILTER_PATH"
 package main
 
 import (
+  "strings"
 	"fmt"
 	"os"
 	"github.com/Masterminds/semver/v3"
 )
 
 func main() {
-	c, _ := semver.NewConstraint(os.Args[1])
-	for _, s := range os.Args[2:] {
+	c, err := semver.NewConstraint(os.Args[1])
+  if err != nil {
+		panic(err)
+	}
+
+	for _, s := range strings.Split(os.Args[2], " ") {
 		if v, _ := semver.NewVersion(s); c.Check(v) {
 			fmt.Println(s + "-SNAPSHOT")
 		}
@@ -34,7 +43,12 @@ func main() {
 GO
 
 # Capture the "returned" array in STACK_VERSIONS
-read -r -a STACK_VERSIONS <<< "$(go run "$SEMVER_FILTER_PATH" "$KIBANA_REQ" $ACTIVE_KIBANA_VERSIONS | xargs)"
+read -r -a STACK_VERSIONS <<< "$(go run "${SEMVER_FILTER_PATH}" "${KIBANA_REQ}" "${ACTIVE_KIBANA_VERSIONS}" | xargs)"
+
+if [[ ! -n "${STACK_VERSIONS+x}" ]]; then
+	echo "There are no active versions satisfying the constraint ${KIBANA_REQ}."
+  exit 0
+fi
 
 # Trigger OOM testing pipeline for each stack version
 for STACK_VERSION in "${STACK_VERSIONS[@]}"
@@ -44,7 +58,7 @@ do
   cat <<YAML | buildkite-agent pipeline upload
 steps:
   - key: 'run-oom-testing-$(echo "$STACK_VERSION" | sed 's/\./_/g')$BUILDKITE_BUILD_NUMBER'
-    label: ":elastic-cloud::bar_chart: [security_detection_engine] Test for OOM issues against ECH $STACK_VERSION"
+    label: ":elastic-cloud::bar_chart: [security_detection_engine] Test for OOM issues against $STACK_VERSION ECH"
     trigger: "appex-qa-stateful-security-prebuilt-rules-ftr-oom-testing"
     async: false
     build:
