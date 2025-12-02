8ba706f1995664fc33bed4a0004698128eccb884
Stijn Holzhauer <stijn.holzhauer@elastic.co>
2025-12-02 08:12:13 +0100

    SKU based chargeback (#16182)
    * adding sku and cost_type to billing data for node granularity
    
    * working on sku with pipeline to do parsing
    
    * downplaying version
    
    * transform

--- packages/chargeback/pre-setup.sh
+++ packages/chargeback/pre-setup.sh
@@ -10,7 +10,7 @@ PUT chargeback_conf_lookup
   "mappings": {
     "_meta": {
       "managed": true,
-      "package": { "name": "chargeback", "version": "0.2.3" }
+      "package": { "name": "chargeback", "version": "0.2.4" }
     },
     "properties": {
       "config_join_key": { "type": "keyword" },
