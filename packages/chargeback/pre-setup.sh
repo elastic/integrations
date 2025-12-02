a279c535c5011b58c69e2fef415938403317ff1a
Stijn Holzhauer <stijn.holzhauer@elastic.co>
2025-12-02 10:42:53 +0100

    setting version

--- packages/chargeback/pre-setup.sh
+++ packages/chargeback/pre-setup.sh
@@ -9,7 +9,7 @@ PUT chargeback_conf_lookup
   "mappings": {
     "_meta": {
       "managed": true,
-      "package": { "name": "chargeback", "version": "0.2.5" }
+      "package": { "name": "chargeback", "version": "0.2.6" }
     },
     "properties": {
       "config_join_key": { "type": "keyword" },
