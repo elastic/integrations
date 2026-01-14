f970c9557e88340a6223cd77a2d7a1364029f112
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-01-14 14:12:51 +0100

    Test subset of packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -829,7 +829,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort |grep -E '^(cloudflare_logpush|elastic_package_registry|netskope)$'
 }
 
 check_package() {
