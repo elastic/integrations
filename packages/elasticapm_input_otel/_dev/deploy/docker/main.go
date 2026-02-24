2771cb223c0ef86f4aee46646773ab3996ac7552
Tere <teresa.romero@elastic.co>
2026-02-24 15:47:43 +0100

    Update main.go to include copyright notice for Elastic License compliance

--- packages/elasticapm_input_otel/_dev/deploy/docker/main.go
+++ packages/elasticapm_input_otel/_dev/deploy/docker/main.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 // HTTP server instrumented with Elastic APM Go agent for system tests.
 // Sends APM Intake v2 data (traces, metrics, logs) to the collector (elastic-agent:8200).
 // Waits for SIGHUP to start load, then runs continuous load until SIGTERM.
