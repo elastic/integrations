464d789b007f79c2d09ba7c0286ae004adad7e20
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-02-24 16:38:15 +0100

    Add first system test just for logs

---
+++ packages/kafka_input_otel/_dev/deploy/docker/zookeeper/startup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# --- Utility Functions ---
+
+# Waits for a specified port to become available on localhost.
+wait_for_port() {
+    count=20
+    port=$1
+    echo "Waiting for port ${port} to become available..."
+    while ! nc -z localhost $port && [[ $count -ne 0 ]]; do
+        count=$(( $count - 1 ))
+        # If the count reaches 0, exit with an error.
+        [[ $count -eq 0 ]] && echo "Error: Port ${port} did not become available." && return 1
+        sleep 0.5
+    done
+    echo "Port ${port} is now available."
+    nc -z localhost $port
+}
+
+echo "Starting ZooKeeper"
+${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
+wait_for_port 2181
+
+# --- Kafka Setup ---
+
+echo "Starting Kafka broker..."
+
+export KAFKA_OPTS="-javaagent:/opt/jolokia/jolokia-jvm.jar=port=8780,host=0.0.0.0"
+
+${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server_custom.properties &
+# Store the Process ID (PID) of the Kafka server
+kafka_pid=$!
+
+# Wait for the Kafka broker and Jolokia agent ports to be available.
+wait_for_port 9092
+wait_for_port 8780
+
+echo "Kafka broker started successfully."
+
+echo "Creating 'test' topic..."
+KAFKA_OPTS="" ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper localhost:2181 --create --if-not-exists --topic my-topic --partitions 1 --replication-factor 1
+
+# --- Start Producer and Consumer ---
+
+echo "Starting producer with Jolokia on port 8775..."
+export KAFKA_OPTS="-javaagent:/opt/jolokia/jolokia-jvm.jar=port=8775,host=0.0.0.0"
+(while true; do echo "Test msg"; sleep 5; done) | \
+${KAFKA_HOME}/bin/kafka-console-producer.sh \
+  --broker-list localhost:9092 \
+  --topic my-topic &
+
+echo "Starting consumer with Jolokia on port 8774..."
+export KAFKA_OPTS="-javaagent:/opt/jolokia/jolokia-jvm.jar=port=8774,host=0.0.0.0"
+${KAFKA_HOME}/bin/kafka-console-consumer.sh \
+  --bootstrap-server localhost:9092 \
+  --topic my-topic \
+  --from-beginning &
+
+# --- Keep Container Running ---
+echo "Kafka setup complete. The container will now run in the background."
+
+# The 'wait' command blocks the script until the Kafka server process (kafka_pid) exits.
+# This keeps the container running.
+wait $kafka_pid
