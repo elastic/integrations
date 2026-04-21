52f34296f3b50662bc850fd218f1da89a08ebf70
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-04-21 11:38:09 +0200

    Fix filtering of packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -829,7 +829,7 @@ teardown_test_package() {
 }
 
 list_all_directories() {
-    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | grep -E '^packages/(elastic_package_registry|nginx|rapid7_insightvm|sql_input|nginx/nginx|nginx/nginx_otel)$'
+    find . -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | sort | grep -E '^(elastic_package_registry|nginx|rapid7_insightvm|sql_input|nginx/nginx|nginx/nginx_otel)$'
 }
 
 check_package() {
