ba2d00427850dae7df386f74fb2701b2f81939aa
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-04-23 19:39:05 +0200

    Add common.sh for testing

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -772,6 +772,7 @@ is_pr_affected() {
         '\.buildkite/scripts/packages/.+\.sh'
         '\.buildkite/scripts/backport_branch\.sh'
         '\.buildkite/scripts/build_packages\.sh'
+        '\.buildkite/scripts/common\.sh'
     )
     local non_package_regex
     non_package_regex="^($(IFS='|'; echo "${non_package_patterns[*]}"))"
