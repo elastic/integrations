78d695e5df7c2953222723221366e03d85f74c2e
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-05-14 20:00:11 +0200

    Test just nginx

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -902,7 +902,7 @@ teardown_test_package() {
 
 # list all directories that are packages from the root of the repository
 list_all_directories() {
-    mage -d "${WORKSPACE}" listPackages
+    mage -d "${WORKSPACE}" listPackages |grep -E '^packages/nginx$'
 }
 
 check_package() {
