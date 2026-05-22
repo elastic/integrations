0c20c86bc8d3d87abedbca133e17433c08980cf6
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-05-22 15:41:20 +0200

    Test subset of packages

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -912,7 +912,7 @@ teardown_test_package() {
 
 # list all directories that are packages from the root of the repository
 list_all_directories() {
-    mage -d "${WORKSPACE}" listPackages
+    mage -d "${WORKSPACE}" listPackages | grep 'elastic_package_registry'
 }
 
 check_package() {
