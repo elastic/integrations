a8135016e4ef633e05ddb40818d11976ca6569ca
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-06-18 18:42:39 +0200

    Skip trigger the integrations tests - to be reverted

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -925,7 +925,7 @@ teardown_test_package() {
 
 # list all directories that are packages from the root of the repository
 list_all_directories() {
-    mage -d "${WORKSPACE}" listPackages |grep -E "^packages/elastic_package_registry$"
+    mage -d "${WORKSPACE}" listPackages
 }
 
 check_package() {
