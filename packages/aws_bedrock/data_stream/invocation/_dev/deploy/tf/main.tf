a451dd27910ff40f08dfe33fe721d6c86855f77a
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-08-13 17:40:32 +0200

    Revert "[aws_bedrock] Add Terraform resource labels"
    This reverts commit e7c955e85c60265a82914bf1e25e0c76ed67eba3.

--- packages/aws_bedrock/data_stream/invocation/_dev/deploy/tf/main.tf
+++ packages/aws_bedrock/data_stream/invocation/_dev/deploy/tf/main.tf
@@ -10,11 +10,6 @@ provider "aws" {
       branch       = var.BRANCH
       build        = var.BUILD_ID
       created_date = var.CREATED_DATE
-
-      division = "engineering"
-      org      = "obs"
-      team     = "security-service-integrations"
-      project  = "integrations-aws_bedrock-package"
     }
   }
 }
